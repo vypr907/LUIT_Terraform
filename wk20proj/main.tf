9db53a81729886bbbb3914c66a035b114a754e22
vypr907 <steven.laszloffy@gmail.com>
2022-07-29 20:33:12 +0000

    fixed availability zone for Copernicus Base

--- wk20proj/main.tf
+++ wk20proj/main.tf
@@ -81,7 +81,7 @@ resource "aws_subnet" "REQ-814-D" {
 resource "aws_subnet" "Copernicus_Base" {
   vpc_id                  = aws_vpc.REQUIEM.id
   cidr_block              = "10.0.3.0/24"
-  availability_zone       = "us-east-2c"
+  availability_zone       = "us-east-2b"
   map_public_ip_on_launch = false
   tags                    = {
     Name = "Copernicus_Base"
